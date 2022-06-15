34dc5ff3a545a227051d1e1d46fddff644b1f0f7
Megumi-Matsui <107203492+Megumi-Matsui@users.noreply.github.com>
2022-06-16 06:27:51 +0900

    カレンダーと右下のボタン追加しました

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,15 @@
 <template>
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
-      <!--ここにお願いします-->
+      <v-calendar type="week"></v-calendar>
+      <v-btn
+        color="primary"
+        elevation="5"
+        fab
+        fixed bottom right
+      >
+        <v-icon>mdi-plus</v-icon>
+      </v-btn>
     </v-col>
   </v-row>
 </template>
